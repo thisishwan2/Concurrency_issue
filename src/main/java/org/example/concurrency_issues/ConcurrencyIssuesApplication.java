618651fdaceda3cbe7381e6ffcd91f82b5058ada
thisishwan2 <feel000617@gmail.com>
2023-11-04 21:45:55 +0900

    initial commit

---
+++ src/main/java/org/example/concurrency_issues/ConcurrencyIssuesApplication.java
@@ -0,0 +1,13 @@
+package org.example.concurrency_issues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyIssuesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrencyIssuesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/concurrency_issues/ConcurrencyIssuesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.concurrency_issues;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrencyIssuesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
