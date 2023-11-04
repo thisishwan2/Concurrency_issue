a7f31210833dd09a71dcfa0e451592aaa9c05ccf
thisishwan2 <feel000617@gmail.com>
2023-11-04 21:55:05 +0900

    Race Condition 발생 테스트 생성

--- src/test/java/org/example/concurrency_issues/service/StockServiceTest.java
+++ src/test/java/org/example/concurrency_issues/service/StockServiceTest.java
@@ -8,6 +8,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -37,5 +41,29 @@ class StockServiceTest {
         // 100-1=99가 되어야 한다.
         Stock stock = stockRepository.findById(1L).orElseThrow();
         assertEquals(99L, stock.getQuantity());
+    }
+
+    @Test
+    public void 동시에_100개의_요창() throws InterruptedException {
+        int threadCount = 100; // 100개의 스레드를 생성한다.
+        ExecutorService executorService = Executors.newFixedThreadPool(32); // 비동기 실행을 단순화하여 실행하도록 도와주는 자바 API
+
+        CountDownLatch latch = new CountDownLatch(threadCount); // 다른 스레드에서 수행중인 작업이 끝날때 대기할 수 있도록 도와주는 클래스이다.
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() ->{
+                    try{
+                        stockService.decrease(1L, 1L);
+                    }finally {
+                        latch.countDown();
+                    }
+            });
+        }
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        // 100-1*100=0이 되어야 한다.
+        assertEquals(0L, stock.getQuantity());
     }
 }
